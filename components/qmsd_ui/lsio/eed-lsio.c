043adadd0b586f7ea225ffad78f761a2b1f45ff0
Enver Haase <enver@vaadin.com>
2024-07-23 21:19:41 +0200

    dry holiday exercise, adding test code for the lsio that may or may not work...

--- components/qmsd_ui/lsio/eed-lsio.c
+++ components/qmsd_ui/lsio/eed-lsio.c
@@ -1,4 +1,13 @@
 #include "eed-lsio.h"
+#include "aw9523.h"
 
 void configure_lsio(void) {
+    // Initialize the AW9523 driver
+    aw9523_init(48, 47);
+
+    aw9523_set_inout(AW9523_PORT_0, AW9523_MODE_OUTPUT);
+    aw9523_set_gpio_or_led(AW9523_PORT_0, AW9523_MODE_GPIO);
+    for (int i=0; i < 8; i++) {
+        aw9523_io_set_level(AW9523_PORT_0, i, i%2);
+    }
 }
